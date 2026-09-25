d205100b8dd4926ca97f21ba337d21a91c1896a6
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-25 14:29:47 +0700

    adding text if code is wrong

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ while (Attempts != secretcode)
 {
     Console.Write("Enter the secret code: ");
     Attempts = Console.ReadLine();
+    if (Attempts != secretcode)
+    {
+        Console.WriteLine("Incorrect code. Please try again.\n");
+    }
 }
 
 Console.WriteLine("Access granted! The mysterious door opens...");
