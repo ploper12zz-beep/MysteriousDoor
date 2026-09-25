b7e66ba2b10bcc25bb25c4809bdbaf55ce06da35
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-25 13:54:30 +0700

    adding system secret code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string secretcode = "1992";
+string Attempts = "";
+
+while (Attempts != secretcode)
+{
+    Console.Write("Enter the secret code: ");
+    Attempts = Console.ReadLine();
+}
+
+Console.WriteLine("Access granted! The mysterious door opens...");
